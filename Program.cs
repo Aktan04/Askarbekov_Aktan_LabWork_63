a96da2a2aec1a658c35d1abddd70dcac12f386ea
Askarbekov Aktan <akn392204@gmail.com>
2024-05-24 18:38:59 +0600

    change master page

--- Program.cs
+++ Program.cs
@@ -49,6 +49,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Chat}/{action=Index}/{id?}");
 
 app.Run();
